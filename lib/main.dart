01cb8bc0ef3c6494ca27f46b513e0bb60e821f42
jaredkc <jared@clivecoffee.com>
2021-08-18 21:34:31 -0600

    Working Algolia example, with infinite scroll

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,35 @@
+import 'package:algolia/algolia.dart';
 import 'package:flutter/material.dart';
 
+// Initiate static Algolia once in your project.
+class Application {
+  static const Algolia algolia = Algolia.init(
+    applicationId: 'latency',
+    apiKey: '1f6fd3a6fb973cb08419fe7d288fa4db',
+  );
+}
+
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Algolia Demo'),
+      home: const MyHomePage(title: 'Flutter Algolia Demo'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
   final String title;
 
   @override
@@ -46,68 +37,162 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  // Initiate Algolia in your project
+  Algolia algolia = Application.algolia;
 
-  void _incrementCounter() {
+  final String _indexName = 'bestbuy';
+  String _searchText = "";
+  int _searchPage = 0;
+  List<SearchHit> _hitsList = [];
+
+  final ScrollController _scrollController = ScrollController();
+  final TextEditingController _searchFieldController = TextEditingController();
+
+  Future<void> _getSearchResult(String query, {bool append = false}) async {
+    AlgoliaQuery response = algolia.instance
+        .index(_indexName)
+        .setPage(_searchPage)
+        .setHitsPerPage(15)
+        .query(query);
+
+    AlgoliaQuerySnapshot snap = await response.getObjects();
+
+    if (!snap.hasHits) {
+      debugPrint('No more results');
+      return;
+    }
+
+    var hitsList = snap.hits.map((hit) {
+      return SearchHit.fromMap(hit);
+    }).toList();
+
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      append ? _hitsList.addAll(hitsList) : _hitsList = hitsList;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        appBar: AppBar(
+          title: const Text('Algolia Search Example'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      body: Column(
+        children: <Widget>[
+          Container(
+            height: 47,
+            padding: const EdgeInsets.symmetric(horizontal: 1),
+            decoration: BoxDecoration(
+              border: Border(
+                bottom: BorderSide(color: Colors.grey.shade300),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            child: TextField(
+              controller: _searchFieldController,
+              decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: 'Enter a search term',
+                  prefixIcon: const Icon(Icons.search),
+                  suffixIcon: _searchText.isNotEmpty
+                      ? IconButton(
+                          onPressed: () {
+                            setState(() {
+                              _searchFieldController.clear();
+                            });
+                          },
+                          icon: const Icon(Icons.clear),
+                        )
+                      : null),
             ),
-          ],
-        ),
+          ),
+          Expanded(
+            child: _hitsList.isEmpty
+                ? const Center(child: Text('No results'))
+                : ListView.builder(
+                    controller: _scrollController,
+                    padding: const EdgeInsets.fromLTRB(0, 0, 0, 64),
+                    itemCount: _hitsList.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Container(
+                        height: 70,
+                        padding: const EdgeInsets.fromLTRB(6, 12, 12, 12),
+                        decoration: BoxDecoration(
+                          border: Border(
+                            bottom: BorderSide(color: Colors.grey.shade300),
+                          ),
+                        ),
+                            child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: <Widget>[
+                              SizedBox(
+                              width: 50,
+                              child: Image.network(_hitsList[index].image),
+                            ),
+                              const SizedBox(width: 10),
+                            Expanded(
+                              child: Text(
+                                _hitsList[index].name,
+                                maxLines: 2,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                              )
+                            ],
+                        ),
+                        );
+                    },
+                  ),
+          )
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _searchFieldController.addListener(() {
+      if (_searchText != _searchFieldController.text) {
+        setState(() {
+          _searchPage = 0;
+          _searchText = _searchFieldController.text;
+        });
+        _getSearchResult(_searchText);
+      }
+    });
+
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels ==
+          _scrollController.position.maxScrollExtent) {
+        setState(() {
+          _searchPage++;
+        });
+        _getSearchResult(_searchText, append: true);
+      }
+    });
+
+    _getSearchResult('');
+  }
+
+  @override
+  void dispose() {
+    _searchFieldController.dispose();
+    _scrollController.dispose();
+    super.dispose();
+  }
+}
+
+class SearchHit {
+  final String name;
+  final String image;
+
+  SearchHit(this.name, this.image);
+
+  static SearchHit fromJson(Map<String, dynamic> json) {
+    return SearchHit(json['name'], json['image']);
+  }
+
+  static SearchHit fromMap(AlgoliaObjectSnapshot hit) {
+    return SearchHit(hit.data['name'], hit.data['image']);
   }
 }
